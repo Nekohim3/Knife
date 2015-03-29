618ac11f19a371d08ca9291bd0d603e0cd44597d
shiramine <kuroneko@protonmail.ch>
2015-03-29 20:13:06 +0300

    1q

--- Knife/MainWindow.xaml.cs
+++ Knife/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +19,21 @@ namespace Knife
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void wb_InitializeView(object sender, Awesomium.Core.WebViewEventArgs e)
+        {
+            wb.WebSession = Awesomium.Core.WebCore.CreateWebSession(System.Environment.CurrentDirectory, new Awesomium.Core.WebPreferences());
+        }
+
+        private void wb_LoadingFrameComplete(object sender, Awesomium.Core.FrameEventArgs e)
+        {
+
         }
     }
 }
