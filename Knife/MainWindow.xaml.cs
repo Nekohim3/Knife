04fa6f4f0620e669bbfff6d9b7e5c45b4c776182
shiramine <kuroneko@protonmail.ch>
2015-03-30 17:53:04 +0300

    zxc

--- Knife/MainWindow.xaml.cs
+++ Knife/MainWindow.xaml.cs
@@ -13,7 +13,12 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using System.Windows.Media.Animation;
+using HtmlAgilityPack;
+using System.IO;
+using System.Data.SQLite;
+using System.Data;
+using System.Threading;
 namespace Knife
 {
     /// <summary>
@@ -25,7 +30,11 @@ namespace Knife
         {
             InitializeComponent();
         }
-
+        string accid = "";
+        bool FirstInit = true;
+        string Cookie = "";
+        string sessionId = "";
+        bool setts = false;
         private void wb_InitializeView(object sender, Awesomium.Core.WebViewEventArgs e)
         {
             wb.WebSession = Awesomium.Core.WebCore.CreateWebSession(System.Environment.CurrentDirectory, new Awesomium.Core.WebPreferences());
@@ -33,7 +42,149 @@ namespace Knife
 
         private void wb_LoadingFrameComplete(object sender, Awesomium.Core.FrameEventArgs e)
         {
+            if(FirstInit)
+                if (wb.Source.AbsoluteUri == "http://steamcommunity.com/market/" || wb.Source.AbsoluteUri == "https://steamcommunity.com/market/")
+                {
+                    HtmlAgilityPack.HtmlDocument doc = new HtmlAgilityPack.HtmlDocument();
+                    doc.LoadHtml(wb.ExecuteJavascriptWithResult("document.documentElement.outerHTML").ToString());
+                    if (doc.DocumentNode.Descendants("span").Where(x => x.Id == "marketWalletBalanceAmount").Count() != 0)
+                    {
+                        double mon = Convert.ToDouble(doc.DocumentNode.Descendants("span").Where(x => x.Id == "marketWalletBalanceAmount").First().InnerText.Split(' ')[0]);
+                        accid = doc.DocumentNode.Descendants("span").Where(c => c.Id == "account_pulldown").First().InnerText;
+                        if(FirstInit)
+                        {
+                            wb.Visibility = System.Windows.Visibility.Hidden;
+                            MW.Width = 640;
+                            MW.Height = 360;
+                            FirstInit = false;
+                            Init();
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please log into steam!");
+                        wb.Visibility = System.Windows.Visibility.Visible;
+                        MW.Width = 1280;
+                        MW.Height = 720;
+                    }
+                }
+        }
+        void Init()
+        {
+            if(!GetCookies())
+            {
+                MessageBox.Show("Error load cookies");
+                return;
+            }
 
         }
+        bool GetCookies()
+        {
+            if (!File.Exists("Cookies"))
+                return false;
+            else
+            {
+                int i = 0;
+                while (i < 3)
+                {
+                    try
+                    {
+
+                        FileStream fs = new FileStream("Cookies", FileMode.Open, FileAccess.Read, FileShare.ReadWrite);
+                        byte[] buf = new byte[fs.Length];
+                        fs.Read(buf, 0, (int)fs.Length);
+                        MemoryStream ms = new MemoryStream(buf);
+                        StreamReader sr = new StreamReader(ms);
+                        string str = sr.ReadToEnd();
+                        sr.Close();
+                        ms.Close();
+                        fs.Close();
+                        sessionId = str.Remove(0, str.IndexOf("steamcommunity.comsessionid")).Substring(0, str.Remove(0, str.IndexOf("steamcommunity.comsessionid")).IndexOf("/")).Replace("steamcommunity.comsessionid", "");
+                        Cookie = "sessionid=" + sessionId + ";";
+                        Cookie += "steamLogin=" + str.Remove(0, str.IndexOf("steamcommunity.comsteamLogin")).Substring(0, str.Remove(0, str.IndexOf("steamcommunity.comsteamLogin")).IndexOf("/")).Replace("steamcommunity.comsteamLogin", "") + ";";
+                        Cookie += "steamLoginSecure=" + str.Remove(0, str.IndexOf("steamcommunity.comsteamLoginSecure")).Substring(0, str.Remove(0, str.IndexOf("steamcommunity.comsteamLoginSecure")).IndexOf("/")).Replace("steamcommunity.comsteamLoginSecure", "") + ";";
+                        Cookie += "comsteamMachineAuth" + str.Remove(0, str.IndexOf("steamcommunity.comsteamMachineAuth")).Substring(0, str.Remove(0, str.IndexOf("steamcommunity.comsteamMachineAuth")).IndexOf("/")).Replace("steamcommunity.comsteamMachineAuth", "").Insert(17, "=") + ";";
+                        return true;
+                    }
+                    catch
+                    {
+                        Thread.Sleep(1000);
+                        i++;
+                        if (i > 5)
+                            return false;
+                    }
+                }
+                return false;
+
+            }
+        }
+        private void MetroWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            sett.Margin = new Thickness(0, 0, -sett.Width, 28);
+            wb.Source = new Uri("http://steamcommunity.com/market/");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (wb.Visibility == System.Windows.Visibility.Hidden)
+                wb.Visibility = System.Windows.Visibility.Visible;
+            else
+                wb.Visibility = System.Windows.Visibility.Hidden;
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            Init();
+        }
+
+        private void MW_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (!setts)
+            {
+                setts = true;
+                //GetCountSlider.Value = Convert.ToDouble(GetCount);
+                //bthsSlider.Value = Convert.ToDouble(bths);
+                //TB_Money.Text = money.ToString();
+                ThicknessAnimation ta = new ThicknessAnimation();
+                ta.From = sett.Margin;
+                ta.To = new Thickness(0, 0, 0, 28);
+                ta.Duration = TimeSpan.FromMilliseconds(500);
+                ta.EasingFunction = new PowerEase()
+                {
+                    EasingMode = EasingMode.EaseOut
+                };
+                sett.BeginAnimation(MarginProperty, ta);
+            }
+            else
+            {
+                setts = false;
+                ThicknessAnimation ta = new ThicknessAnimation();
+                ta.From = sett.Margin;
+                ta.To = new Thickness(0, 0, -sett.Width, 28);
+                ta.Duration = TimeSpan.FromMilliseconds(500);
+                ta.EasingFunction = new PowerEase()
+                {
+                    EasingMode = EasingMode.EaseOut
+                };
+                sett.BeginAnimation(MarginProperty, ta);
+            }
+        }
+    }
+    public enum State
+    {
+        search,
+        get,
+        banned,
+        message,
+        buying
     }
 }
