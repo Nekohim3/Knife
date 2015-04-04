5a17d6cd706b4a782f5b0c8ebda20b27a9d1d54c
shiramine <kuroneko@protonmail.ch>
2015-04-05 01:08:25 +0300

    asdf

--- Knife/MainWindow.xaml.cs
+++ Knife/MainWindow.xaml.cs
@@ -20,7 +20,6 @@ using System.Data.SQLite;
 using System.Data;
 using System.Threading;
 using System.Runtime.InteropServices;
-using WpfAnimatedGif;
 using System.Windows.Threading;
 namespace Knife
 {
@@ -32,7 +31,7 @@ namespace Knife
         public MainWindow()
         {
             InitializeComponent();
-            states = new States(WFH_Img_ConnectToServer, WFH_Img_SteamAuth);
+            states = new States(Img_ConnectToServer, Img_SteamAuth);
             client.Connected += client_Connected;
             client.Disconnected += client_Disconnected;
             client.PacketReceived += client_PacketReceived;
@@ -51,24 +50,13 @@ namespace Knife
         string ServerPort = "18346";
         public class States
         {
-            System.Windows.Forms.Integration.WindowsFormsHost WFH_Img_ConnectToServer;
-            System.Windows.Forms.Integration.WindowsFormsHost WFH_Img_SteamAuth;
-            public States(System.Windows.Forms.Integration.WindowsFormsHost img1, System.Windows.Forms.Integration.WindowsFormsHost img2)
+            Image Img_ConnectToServer;
+            Image Img_SteamAuth;
+            public States(Image img1, Image img2)
             {
-                WFH_Img_ConnectToServer = img1;
-                WFH_Img_SteamAuth = img2;
+                Img_ConnectToServer = img1;
+                Img_SteamAuth = img2;
             }
-
-            System.Windows.Forms.ToolTip tt_ConnectToServer = new System.Windows.Forms.ToolTip()
-            {
-                BackColor = System.Drawing.Color.FromArgb(255, 15, 15, 15),
-                ForeColor = System.Drawing.Color.FromArgb(255, 200, 200, 200)
-            };
-            System.Windows.Forms.ToolTip tt_SteamAuth = new System.Windows.Forms.ToolTip()
-            {
-                BackColor = System.Drawing.Color.FromArgb(255, 15, 15, 15),
-                ForeColor = System.Drawing.Color.FromArgb(255, 200, 200, 200)
-            };
 
             private ConnState _ClientState;
             public ConnState ClientState
@@ -82,26 +70,38 @@ namespace Knife
                     _ClientState = value;
                     if (_ClientState == ConnState.Connected)
                     {
-                        WFH_Img_ConnectToServer.Dispatcher.Invoke(new Action(() =>
+                        Img_ConnectToServer.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "ok.png");
-                            tt_ConnectToServer.SetToolTip(WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox, "Connected");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "ok.png");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_ConnectToServer, image);
+                            Img_SteamAuth.ToolTip = "Connected";
                         }));
                     }
                     if (_ClientState == ConnState.Connecting)
                     {
-                        WFH_Img_ConnectToServer.Dispatcher.Invoke(new Action(() =>
+                        Img_ConnectToServer.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "loading.gif");
-                            tt_ConnectToServer.SetToolTip(WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox, "Connecting");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "loading.gif");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_ConnectToServer, image);
+                            Img_SteamAuth.ToolTip = "Connecting";
                         }));
                     }
                     if (_ClientState == ConnState.Disconnected)
                     {
-                        WFH_Img_ConnectToServer.Dispatcher.Invoke(new Action(() =>
+                        Img_ConnectToServer.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "error.png");
-                            tt_ConnectToServer.SetToolTip(WFH_Img_ConnectToServer.Child as System.Windows.Forms.PictureBox, "Disconnected");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "error.png");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_ConnectToServer, image);
+                            Img_SteamAuth.ToolTip = "Disconnected";
                         }));
                     }
                 }
@@ -122,34 +122,50 @@ namespace Knife
                         _steamAuthState = SteamAuthState.NotAuth;
                     if (_steamAuthState == SteamAuthState.Auth)
                     {
-                        WFH_Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
+                        Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "ok.png");
-                            tt_SteamAuth.SetToolTip(WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox, "Auth");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "ok.png");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_SteamAuth, image);
+                            Img_SteamAuth.ToolTip = "Auth";
                         }));
                     }
                     if (_steamAuthState == SteamAuthState.Authing)
                     {
-                        WFH_Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
+                        Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "loading.gif");
-                            tt_SteamAuth.SetToolTip(WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox, "Authing");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "loading.gif");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_SteamAuth, image);
+                            Img_SteamAuth.ToolTip = "Authing";
                         }));
                     }
                     if (_steamAuthState == SteamAuthState.NotAuth)
                     {
-                        WFH_Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
+                        Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "error.png");
-                            tt_SteamAuth.SetToolTip(WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox, "NotAuth");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "error.png");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_SteamAuth, image);
+                            Img_SteamAuth.ToolTip = "NotAuth";
                         }));
                     }
                     if (_steamAuthState == SteamAuthState.NotLogged)
                     {
-                        WFH_Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
+                        Img_SteamAuth.Dispatcher.BeginInvoke(DispatcherPriority.Background, new Action(delegate
                         {
-                            (WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox).Image = System.Drawing.Image.FromFile(AppDomain.CurrentDomain.BaseDirectory + "warning.png");
-                            tt_SteamAuth.SetToolTip(WFH_Img_SteamAuth.Child as System.Windows.Forms.PictureBox, "You are not logged in steam. Please click here to login");
+                            var image = new BitmapImage();
+                            image.BeginInit();
+                            image.UriSource = new Uri(AppDomain.CurrentDomain.BaseDirectory + "warning.png");
+                            image.EndInit();
+                            WpfAnimatedGif.ImageBehavior.SetAnimatedSource(Img_SteamAuth, image);
+                            Img_SteamAuth.ToolTip = "You are not logged in steam. Please click here to login";
                         }));
                     }
                 }
@@ -339,35 +355,35 @@ namespace Knife
         }
         private void Settings_Button_Click(object sender, RoutedEventArgs e)
         {
-            if (!setts)
-            {
-                setts = true;
-                //GetCountSlider.Value = Convert.ToDouble(GetCount);
-                //bthsSlider.Value = Convert.ToDouble(bths);
-                //TB_Money.Text = money.ToString();
-                ThicknessAnimation ta = new ThicknessAnimation();
-                ta.From = sett.Margin;
-                ta.To = new Thickness(0, 0, 0, 30);
-                ta.Duration = TimeSpan.FromMilliseconds(500);
-                ta.EasingFunction = new PowerEase()
-                {
-                    EasingMode = EasingMode.EaseOut
-                };
-                sett.BeginAnimation(MarginProperty, ta);
-            }
-            else
-            {
-                setts = false;
-                ThicknessAnimation ta = new ThicknessAnimation();
-                ta.From = sett.Margin;
-                ta.To = new Thickness(0, 0, -sett.Width, 30);
-                ta.Duration = TimeSpan.FromMilliseconds(500);
-                ta.EasingFunction = new PowerEase()
-                {
-                    EasingMode = EasingMode.EaseOut
-                };
-                sett.BeginAnimation(MarginProperty, ta);
-            }
+            //if (!setts)
+            //{
+            //    setts = true;
+            //    //GetCountSlider.Value = Convert.ToDouble(GetCount);
+            //    //bthsSlider.Value = Convert.ToDouble(bths);
+            //    //TB_Money.Text = money.ToString();
+            //    ThicknessAnimation ta = new ThicknessAnimation();
+            //    ta.From = sett.Margin;
+            //    ta.To = new Thickness(0, 0, 0, 30);
+            //    ta.Duration = TimeSpan.FromMilliseconds(500);
+            //    ta.EasingFunction = new PowerEase()
+            //    {
+            //        EasingMode = EasingMode.EaseOut
+            //    };
+            //    sett.BeginAnimation(MarginProperty, ta);
+            //}
+            //else
+            //{
+            //    setts = false;
+            //    ThicknessAnimation ta = new ThicknessAnimation();
+            //    ta.From = sett.Margin;
+            //    ta.To = new Thickness(0, 0, -sett.Width, 30);
+            //    ta.Duration = TimeSpan.FromMilliseconds(500);
+            //    ta.EasingFunction = new PowerEase()
+            //    {
+            //        EasingMode = EasingMode.EaseOut
+            //    };
+            //    sett.BeginAnimation(MarginProperty, ta);
+            //}
         }
         private void Img_SteamAuth_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
